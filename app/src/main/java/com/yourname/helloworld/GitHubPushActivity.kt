f80f57f0f94d77356fd51aaa7e6744e4f657ef89
Hackathon <hackathon@example.com>
2026-05-30 05:51:38 +0000

    Fix OpenSshConfig import for JGit

--- app/src/main/java/com/yourname/helloworld/GitHubPushActivity.kt
+++ app/src/main/java/com/yourname/helloworld/GitHubPushActivity.kt
@@ -17,10 +17,10 @@ import kotlinx.coroutines.withContext
 import org.eclipse.jgit.api.Git
 import org.eclipse.jgit.api.TransportConfigCallback
 import org.eclipse.jgit.lib.Constants
-import org.eclipse.jgit.transport.OpenSshConfig
 import org.eclipse.jgit.transport.SshTransport
 import org.eclipse.jgit.transport.Transport
 import org.eclipse.jgit.transport.ssh.jsch.JschConfigSessionFactory
+import org.eclipse.jgit.transport.ssh.jsch.OpenSshConfig
 import org.eclipse.jgit.util.FS
 import com.jcraft.jsch.JSch
 import com.jcraft.jsch.Session
